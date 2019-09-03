e53d91f9015fbbccccfb7d61b7d3590dfe4d543a
daeroro <skseofh@naver.com>
2019-09-03 19:40:22 +0900

    update comment

--- linux-5.1.15/scripts/dtc/libfdt/fdt.c
+++ linux-5.1.15/scripts/dtc/libfdt/fdt.c
@@ -60,18 +60,30 @@
  * that the given buffer contains what appears to be a flattened
  * device tree with sane information in its header.
  */
+/*
+	fdt_ro_probe_() : read only인지 확인하는 함수
+*/
 int fdt_ro_probe_(const void *fdt)
 {
+	//magic_number 확인, FDT_MAGIC = 0xd00dfeed
 	if (fdt_magic(fdt) == FDT_MAGIC) {
 		/* Complete tree */
+		// 버전 확인
+		// FDT_FIRST_SUPPORTED_VERSION = 0x02
 		if (fdt_version(fdt) < FDT_FIRST_SUPPORTED_VERSION)
 			return -FDT_ERR_BADVERSION;
+		// FDT_LAST_SUPPORTED_VERSION = 0x11
 		if (fdt_last_comp_version(fdt) > FDT_LAST_SUPPORTED_VERSION)
 			return -FDT_ERR_BADVERSION;
+
+	// FDT_SW_MAGIC = (~FDT_MAGIC)
+	// magic number를 확인해서 FDT_SW_MAGIC이면 struct 사이즈를 확인함
 	} else if (fdt_magic(fdt) == FDT_SW_MAGIC) {
 		/* Unfinished sequential-write blob */
 		if (fdt_size_dt_struct(fdt) == 0)
 			return -FDT_ERR_BADSTATE;
+
+	// magic number가 맞지 않으면 에러 return
 	} else {
 		return -FDT_ERR_BADMAGIC;
 	}
@@ -79,23 +91,38 @@ int fdt_ro_probe_(const void *fdt)
 	return 0;
 }
 
+/*
+	check_off_() : offset이 DTB의 header와 끝 사이에 있는 것인지 확인
+*/
+
 static int check_off_(uint32_t hdrsize, uint32_t totalsize, uint32_t off)
 {
 	return (off >= hdrsize) && (off <= totalsize);
 }
 
+/*
+	check_block_() : block의 base, base+size가 DTB의 header~ DTB의 끝 사이에 있는지 확인
+*/
 static int check_block_(uint32_t hdrsize, uint32_t totalsize,
 			uint32_t base, uint32_t size)
 {
+	// base가 DTB의 header~끝의 범위를 벗어난 경우
 	if (!check_off_(hdrsize, totalsize, base))
 		return 0; /* block start out of bounds */
+
+	// size < 0 인 경우
 	if ((base + size) < base)
 		return 0; /* overflow */
+
+	// block이 범위를 범어난 경우
 	if (!check_off_(hdrsize, totalsize, base + size))
 		return 0; /* block end out of bounds */
 	return 1;
 }
 
+/*
+	fdt_header_size_() : 버전에 따른 header 사이즈를 return
+*/
 size_t fdt_header_size_(uint32_t version)
 {
 	if (version <= 1)
@@ -110,19 +137,32 @@ size_t fdt_header_size_(uint32_t version)
 		return FDT_V17_SIZE;
 }
 
+/*
+	fdt_check_header() : header 구조체에 정의되어 있는 정보들
+						 (magic number, header size, version, totalsize, reserve block 크기, 
+						  structure block 크기, string block 크기)을 확인함.
+*/
+
 int fdt_check_header(const void *fdt)
 {
 	size_t hdrsize;
 
+	// magic number 확인
 	if (fdt_magic(fdt) != FDT_MAGIC)
 		return -FDT_ERR_BADMAGIC;
+
+	// header size 저장
 	hdrsize = fdt_header_size(fdt);
+
+	// version 확인
+	// 현재 버전과 호환될 수 있는 버전도 확인
 	if ((fdt_version(fdt) < FDT_FIRST_SUPPORTED_VERSION)
 	    || (fdt_last_comp_version(fdt) > FDT_LAST_SUPPORTED_VERSION))
 		return -FDT_ERR_BADVERSION;
 	if (fdt_version(fdt) < fdt_last_comp_version(fdt))
 		return -FDT_ERR_BADVERSION;
 
+	// fdt의 전체 크기확인
 	if ((fdt_totalsize(fdt) < hdrsize)
 	    || (fdt_totalsize(fdt) > INT_MAX))
 		return -FDT_ERR_TRUNCATED;
@@ -151,23 +191,38 @@ int fdt_check_header(const void *fdt)
 	return 0;
 }
 
+/*
+	fdt_offset_ptr() : structure에서 offset만큼 거리에 있는 node의 포인터를 구함
+*/
 const void *fdt_offset_ptr(const void *fdt, int offset, unsigned int len)
 {
+	// offset은 structure에서부터 찾는 node까지의 offset을 의미
+	// absoffset는 찾는 node의 offset을 의미
 	unsigned absoffset = offset + fdt_off_dt_struct(fdt);
 
+	// 범위 검사
+	// structure offset이 음수인지, len가 음수인지, 전체 fdt 사이즈를 넘는 지
 	if ((absoffset < offset)
 	    || ((absoffset + len) < absoffset)
 	    || (absoffset + len) > fdt_totalsize(fdt))
 		return NULL;
 
+	// 17버전 이후에는 header에 structure block의 사이즈를 나타내는 size_dt_struct가 추가됨
+	// 범위 검사
+    // len이 음수 인지, node의 크기가 structure의 사이즈를 넘는지
 	if (fdt_version(fdt) >= 0x11)
 		if (((offset + len) < offset)
 		    || ((offset + len) > fdt_size_dt_struct(fdt)))
 			return NULL;
-
+	
+	// 찾는 structure node을 가리키는 포인터 반환
 	return fdt_offset_ptr_(fdt, offset);
 }
 
+/*
+	fdt_next_tag() : nextoffset에 현재 startoffset이 가리키는 tag의 다음 tag를 저장하고
+					 현재 tag에 저장된 값을 반환
+*/
 uint32_t fdt_next_tag(const void *fdt, int startoffset, int *nextoffset)
 {
 	const fdt32_t *tagp, *lenp;
@@ -175,36 +230,64 @@ uint32_t fdt_next_tag(const void *fdt, int startoffset, int *nextoffset)
 	int offset = startoffset;
 	const char *p;
 
+	// 일단 err로 설정
 	*nextoffset = -FDT_ERR_TRUNCATED;
+	// structure에서 offset(startoffset)만큼 떨어진 곳에 있는 node의 포인터를 저장
 	tagp = fdt_offset_ptr(fdt, offset, FDT_TAGSIZE);
+	
+	// NULL이면 node가 범위를 벗어난 것임
 	if (!tagp)
 		return FDT_END; /* premature end */
+	// 엔디안에 맞게 tagp에 저장되어 있는 값을 가져옴.
 	tag = fdt32_to_cpu(*tagp);
+	// FDT_TAGSIZE = 4
+	// tag의 끝을 나타내는 offset이 됨
 	offset += FDT_TAGSIZE;
 
 	*nextoffset = -FDT_ERR_BADSTRUCTURE;
+	// 현재 tag가 BEGIN_NODE, PROP, END, END_NODE, NOP인지에 따라 
+	// 다음 tag를 구하는 연산을 함
 	switch (tag) {
 	case FDT_BEGIN_NODE:
+		// 현재 tag가 BEGIN_NODE이면 다음 tag를 구하기 위해서는 name을 건너뛰어야함
 		/* skip name */
+
+		// '\0'을 만날때 까지 offset을 1씩 더하면서 검사
 		do {
 			p = fdt_offset_ptr(fdt, offset++, 1);
 		} while (p && (*p != '\0'));
+		// p가 NULL이면 범위를 벗어난 것
 		if (!p)
 			return FDT_END; /* premature end */
 		break;
 
 	case FDT_PROP:
+		// 현재 tag가 PROP이면 다음 tag구하려면 value length, name offset, value를 건너뛰어야 함
+		// lenp에 현재 tag의 value length를 가리키는 포인터 저장
 		lenp = fdt_offset_ptr(fdt, offset, sizeof(*lenp));
+
 		if (!lenp)
 			return FDT_END; /* premature end */
 		/* skip-name offset, length and value */
+
+		// fdt_property는 tag, len, nameoff로 이루어짐 -> 12byte
+		// FDT_TAGSIZE = 4
+		// fdt32_to_cpu(*lenp) : 현재 tag의 value length
+
+		// offset += (tag, len, nameoff) - tag size + value length
+		// 따라서 다음 tag offset를 나타내는 offset이 됨
 		offset += sizeof(struct fdt_property) - FDT_TAGSIZE
 			+ fdt32_to_cpu(*lenp);
+
+		// 15버전 이하이고, value length가 8이상,
+		// tag, value length, name off이 8단위로 정렬되있지 않으면 offset에 4를 더함
+
+		// value가 8이상이면 공간을 더 마련해주기 위해서 하는 것 같음...
 		if (fdt_version(fdt) < 0x10 && fdt32_to_cpu(*lenp) >= 8 &&
 		    ((offset - fdt32_to_cpu(*lenp)) % 8) != 0)
 			offset += 4;
 		break;
-
+	// tag가 FDT_END, END_NODE, NOP일 때는 다음 tag를 구하지 않음
 	case FDT_END:
 	case FDT_END_NODE:
 	case FDT_NOP:
@@ -214,13 +297,19 @@ uint32_t fdt_next_tag(const void *fdt, int startoffset, int *nextoffset)
 		return FDT_END;
 	}
 
+	// startoffset에서 부터 offset - startoffset 만큼 크기를 가질 수 있는지 범위검사
 	if (!fdt_offset_ptr(fdt, startoffset, offset - startoffset))
 		return FDT_END; /* premature end */
 
+	// tag size(4)로 offset을 정렬하여 nextoffset에 저장
 	*nextoffset = FDT_TAGALIGN(offset);
 	return tag;
 }
 
+/*
+	fdt_check_node_offset() : offset이 음수인지, tag size단위로 정렬되어있는 지, 
+								BEGIN_NODE를 가리키고 있는지 확인
+*/
 int fdt_check_node_offset_(const void *fdt, int offset)
 {
 	if ((offset < 0) || (offset % FDT_TAGSIZE)
@@ -230,6 +319,10 @@ int fdt_check_node_offset_(const void *fdt, int offset)
 	return offset;
 }
 
+/*
+	fdt_check_prop_offset() : offset이 음수인지, tag size단위로 정렬되어 있는 지,
+								PROP을 가리키고 있는지 확인
+*/
 int fdt_check_prop_offset_(const void *fdt, int offset)
 {
 	if ((offset < 0) || (offset % FDT_TAGSIZE)
@@ -239,57 +332,86 @@ int fdt_check_prop_offset_(const void *fdt, int offset)
 	return offset;
 }
 
+/*
+	fdt_next_node() : 현재 offset의 depth를 가지고 tag단위로 확인
+					  다음 node의 offset을 반환
+*/
 int fdt_next_node(const void *fdt, int offset, int *depth)
 {
 	int nextoffset = 0;
 	uint32_t tag;
 
+	// 현재 offset을 범위, 정렬, BEGIN_NODE를 가리키고 있는지 검사
 	if (offset >= 0)
 		if ((nextoffset = fdt_check_node_offset_(fdt, offset)) < 0)
 			return nextoffset;
+			
 
 	do {
 		offset = nextoffset;
+		// 현재 offset의 tag를 tag에 저장,
+		// 다음 tag의 offset을 nextoffset에 저장
 		tag = fdt_next_tag(fdt, offset, &nextoffset);
 
 		switch (tag) {
+		//현재 tag가 PROP, NOP이면 신경 안씀
 		case FDT_PROP:
 		case FDT_NOP:
 			break;
-
+		
+		// 현재 tag가 BEGIN_NODE 이면, depth가 NULL이 아니면 depth를 증가시킴
+		// 현재 tag가 BEGIN_NODE 이면 do_while 문을 빠져나감
 		case FDT_BEGIN_NODE:
 			if (depth)
 				(*depth)++;
 			break;
-
+		
+		// 현재 tag가 END_NODE이면, 
 		case FDT_END_NODE:
+			// depth < 0 이면 루트 노드를 벗어난 것이므로
+			// fdt_next_tag()에서 nextoffset에 에러 값이 저장되어 있을 것이으로 에러 반환
 			if (depth && ((--(*depth)) < 0))
 				return nextoffset;
 			break;
-
+		
+		// 현재 tag가 END이면, 다음 node가 존재 하지 않으므로
 		case FDT_END:
+			// nextoffset >= 0 이면 다음 tag가 존재한다는 의미이므로 에러
+			// fdt_next_tag에서 nextoffset이 -FDT_ERR_TRUNCATED이면 범위를 벗어난 것
 			if ((nextoffset >= 0)
 			    || ((nextoffset == -FDT_ERR_TRUNCATED) && !depth))
 				return -FDT_ERR_NOTFOUND;
 			else
 				return nextoffset;
 		}
+	//현재 tag가 BEGIN_NODE 일 때까지 반복
 	} while (tag != FDT_BEGIN_NODE);
 
+	// BEGIN_NODE 일때의 offset 반환
 	return offset;
 }
 
+/*
+	fdt_first_subnode() : 현재 offset이 가리키는 tag로 부터 다음 첫번째 자식 node를 찾음
+							자식 노드가 없으면 err
+*/
 int fdt_first_subnode(const void *fdt, int offset)
 {
 	int depth = 0;
-
+	
+	// 현재 offset이 가리키는 tag로 부터 depth = 0을 가지고 다음 노드를 찾음
 	offset = fdt_next_node(fdt, offset, &depth);
+	// offset < 0 이면 err
+	// depth가 1이 아니면, 자식 node가 아님
 	if (offset < 0 || depth != 1)
 		return -FDT_ERR_NOTFOUND;
 
 	return offset;
 }
 
+/*
+	fdt_next_subnode() : 현재 offset이 가리키는 node의 형제 node를 찾음
+*/
 int fdt_next_subnode(const void *fdt, int offset)
 {
 	int depth = 1;
@@ -298,34 +420,56 @@ int fdt_next_subnode(const void *fdt, int offset)
 	 * With respect to the parent, the depth of the next subnode will be
 	 * the same as the last.
 	 */
+
+	// 자식 node가 존재하면 depth 증가
+	// 부모 node로 올라가면 depth 감소
+
 	do {
+		// 다음 node의 offset을 구해서 offset에 저장
 		offset = fdt_next_node(fdt, offset, &depth);
+		// offset < 0이면 다음 node가 없다는 의미
+		// depth < 1 이면 부모 node로 올라간 것, 형제 node가 없다는 의미
 		if (offset < 0 || depth < 1)
 			return -FDT_ERR_NOTFOUND;
+
+	// 자식 node가 존재하는 경우에는 계속 다음 node를 찾는 것을 반복
 	} while (depth > 1);
 
 	return offset;
 }
 
+/*
+	fdt_find_string_() : string block에서 s를 찾아서 존재하면 포인터 반환, 
+													존재하지 않으면 NULL 반환
+*/
 const char *fdt_find_string_(const char *strtab, int tabsize, const char *s)
 {
+	// NULL 문자를 포함한 s길이 저장
 	int len = strlen(s) + 1;
+	// string block의 마지막에서 s의 길이만큼을 뺀 곳까지의 포인터 저장
 	const char *last = strtab + tabsize - len;
 	const char *p;
 
+	// string block의 시작부터 last까지 한문자씩 옮겨가며 s와 비교하여 포인터 반환
 	for (p = strtab; p <= last; p++)
 		if (memcmp(p, s, len) == 0)
 			return p;
 	return NULL;
 }
 
+/*
+	fdt_move() : fdt 전체를 buf에 복사
+*/
 int fdt_move(const void *fdt, void *buf, int bufsize)
 {
+	// fdt가 read only인지 확인
 	FDT_RO_PROBE(fdt);
 
+	// buffer 사이즈가 fdt의 전체 크기보다 작으면 err
 	if (fdt_totalsize(fdt) > bufsize)
 		return -FDT_ERR_NOSPACE;
 
+	// fdt에서 buf로 fdt_totalsize(fdt) 만큼 복사
 	memmove(buf, fdt, fdt_totalsize(fdt));
 	return 0;
 }
--- linux-5.1.15/scripts/dtc/libfdt/fdt_ro.c
+++ linux-5.1.15/scripts/dtc/libfdt/fdt_ro.c
@@ -55,52 +55,84 @@
 
 #include "libfdt_internal.h"
 
+/*
+	fdt_nodename_eq_() : 찾고자 하는 node의 offset, 이름 s, 길이 len에
+						일치하는 node가 있는지 확인
+*/
+
 static int fdt_nodename_eq_(const void *fdt, int offset,
 			    const char *s, int len)
 {
 	int olen;
+	// offset에 해당하는 node의 name을 가리키는 포인터를 p에 저장
+	// name의 길이는 olen에 저장
 	const char *p = fdt_get_name(fdt, offset, &olen);
 
+	// p가 NULL 이거나
+	// olen < len 이면 0을 반환
 	if (!p || olen < len)
 		/* short match */
 		return 0;
 
+	// memcmp()는 p, s를 len만큼 길이로 비교해서 일치하면 0, 
+	//											일치하지 않으면 0이 아닌 수를 반환
 	if (memcmp(p, s, len) != 0)
 		return 0;
 
+	// p의 마지막 문자가 '\0'이면 1을 반환
 	if (p[len] == '\0')
-		return 1;
+		return 1;'
+	// '@'문자가 s안에 없거나
+	// p의 마지막 문자가 '@'이면 1을 반환
 	else if (!memchr(s, '@', len) && (p[len] == '@'))
 		return 1;
 	else
 		return 0;
 }
 
+
+/*
+	fdt_get_string() : stroffset에 해당하는 string을 반환하고 길이를 lenp에 저장
+*/
 const char *fdt_get_string(const void *fdt, int stroffset, int *lenp)
 {
+	// string block에서 stroffset에 해당하는 offset을 absoffset에 저장
 	uint32_t absoffset = stroffset + fdt_off_dt_strings(fdt);
 	size_t len;
 	int err;
 	const char *s, *n;
 
+	// read only인지 확인, 맞으면 0을 반환
 	err = fdt_ro_probe_(fdt);
 	if (err != 0)
 		goto fail;
-
+	
+	// 우선 err에 -FDT_ERR_BADOFFSET 값 설정
 	err = -FDT_ERR_BADOFFSET;
+	// absoffset이 전체 fdt 범위 안에 있는지 확인
 	if (absoffset >= fdt_totalsize(fdt))
 		goto fail;
+	// absoffset 부터 fdt 전체 범위까지의 길이 저장
 	len = fdt_totalsize(fdt) - absoffset;
 
+	// magic number 확인
 	if (fdt_magic(fdt) == FDT_MAGIC) {
+		// stroffset 확인
 		if (stroffset < 0)
 			goto fail;
+		// 버전이 17이상이면 
 		if (fdt_version(fdt) >= 17) {
+			
+			// stroffset이 전체 strings block 크기를 넘어가는지 확인
 			if (stroffset >= fdt_size_dt_strings(fdt))
 				goto fail;
+
+			// string block 크기 - stroffset < len 
+			//			: stroffset이 stirng block 끝 쪽에 위치한다는 의미
 			if ((fdt_size_dt_strings(fdt) - stroffset) < len)
 				len = fdt_size_dt_strings(fdt) - stroffset;
 		}
+	// sw magic number이면, stroffset을 음수로 취급하는 것 같음.
 	} else if (fdt_magic(fdt) == FDT_SW_MAGIC) {
 		if ((stroffset >= 0)
 		    || (stroffset < -fdt_size_dt_strings(fdt)))
@@ -112,57 +144,80 @@ const char *fdt_get_string(const void *fdt, int stroffset, int *lenp)
 		goto fail;
 	}
 
+	// fdt부터 absoffset에 해당하는 문자열을 가리키는 포인터를 s에 저장
 	s = (const char *)fdt + absoffset;
+	// 문자열 s에서 len 길이만큼 '\0'문자 찾기
 	n = memchr(s, '\0', len);
+	// n=0, '\0'문자를 찾지 못했으면 에러
 	if (!n) {
 		/* missing terminating NULL */
 		err = -FDT_ERR_TRUNCATED;
 		goto fail;
 	}
-
+	
+	// lenp가 NULL이 아니면, lenp이 가리키는 곳에 n-s, 문자열의 길이 저장하고 s를 반환
 	if (lenp)
 		*lenp = n - s;
 	return s;
 
 fail:
+	// 찾지 못했을 경우에는 lenp가 가리키는 곳에 err를 저장하고 NULL을 반환
 	if (lenp)
 		*lenp = err;
 	return NULL;
 }
 
+/*
+	fdt_string() : string block에서 stroffset에 해당하는 string을 반환
+*/
 const char *fdt_string(const void *fdt, int stroffset)
 {
 	return fdt_get_string(fdt, stroffset, NULL);
 }
 
+/*
+	fdt_string_eq_() : stroffset에 해당하는 문자열과
+						 len의 길이를 가지는 문자열 s가 같은지 비교
+*/
 static int fdt_string_eq_(const void *fdt, int stroffset,
 			  const char *s, int len)
 {
 	int slen;
+	// stroffset에 해당하는 string을 반환, slen에 길이 저장
 	const char *p = fdt_get_string(fdt, stroffset, &slen);
 
+	// 문자열이 존재하고, 길이가 같고, 내용이 같으면 1을 반환
 	return p && (slen == len) && (memcmp(p, s, len) == 0);
 }
 
+/*
+	fdt_get_max_phandle() : 디바이스 트리에서 phandle이 가장 큰 값을 반환
+*/
 uint32_t fdt_get_max_phandle(const void *fdt)
 {
 	uint32_t max_phandle = 0;
 	int offset;
 
+	// 처음 node에서 마지막 노드까지 검사
 	for (offset = fdt_next_node(fdt, -1, NULL);;
 	     offset = fdt_next_node(fdt, offset, NULL)) {
 		uint32_t phandle;
 
+		// fdt_nex_node의 값이 -FDT_ERR_NOTFOUND는 현재 node가 FDT_END라는 의미
 		if (offset == -FDT_ERR_NOTFOUND)
 			return max_phandle;
 
+		// offset이 음수이면 에러
 		if (offset < 0)
 			return (uint32_t)-1;
 
+		// offset에 해당하는 node에서 phandler을 찾음
 		phandle = fdt_get_phandle(fdt, offset);
+		// phandler을 찾지못하면 continue;
 		if (phandle == (uint32_t)-1)
 			continue;
-
+		
+		// 가장 큰 값을 max_phandler에 저장
 		if (phandle > max_phandle)
 			max_phandle = phandle;
 	}
@@ -170,37 +225,62 @@ uint32_t fdt_get_max_phandle(const void *fdt)
 	return 0;
 }
 
+/*
+	fdt_mem_rsv() : n번째에 해당하는 reserve_entry를 반환
+*/
 static const struct fdt_reserve_entry *fdt_mem_rsv(const void *fdt, int n)
 {
+	// n번째 reserve_entry의 offset을 저장
 	int offset = n * sizeof(struct fdt_reserve_entry);
+	// fdt에서 n번째 reserve_entry의 offset을 저장
 	int absoffset = fdt_off_mem_rsvmap(fdt) + offset;
 
+	// absoffset의 범위검사
+	// absoffset이 reserve blcok 보다 작을 수 없음
 	if (absoffset < fdt_off_mem_rsvmap(fdt))
 		return NULL;
+	// structure block, string block이 없을 때,
+	// absoffset이 가질수 있는 최대 범위 검사
 	if (absoffset > fdt_totalsize(fdt) - sizeof(struct fdt_reserve_entry))
 		return NULL;
+
+	// fdt의 reserve block에서 n번째의 fdt_reserve_entry 반환
 	return fdt_mem_rsv_(fdt, n);
 }
 
+/*
+	fdt_get_mem_rsv() : n번째에 해당하는 fdt_reserve_entry의
+						 주소를	address가 가리키는 곳에 저장,
+						 크기를 size가 가리키는 곳에 저장
+*/
 int fdt_get_mem_rsv(const void *fdt, int n, uint64_t *address, uint64_t *size)
 {
+	// fdt_reserve_entry 구조체 포인터 선언
 	const struct fdt_reserve_entry *re;
 
 	FDT_RO_PROBE(fdt);
+	// n번째 reserve_entry 구조체 저장
 	re = fdt_mem_rsv(fdt, n);
+	// re = NULL이면 에러
 	if (!re)
 		return -FDT_ERR_BADOFFSET;
 
+	// re 구조체의 주소, 크기를 엔디안을 고려해 저장
 	*address = fdt64_ld(&re->address);
 	*size = fdt64_ld(&re->size);
 	return 0;
 }
 
+/*
+	fdt_num_mem_rsv() : reserve_entry의 개수를 반환
+*/
 int fdt_num_mem_rsv(const void *fdt)
 {
 	int i;
+	// reserve_entry 구조체 선언
 	const struct fdt_reserve_entry *re;
 
+	// i=0 부터 fdt_mem_rsv()가 NULL일 때까지 반복
 	for (i = 0; (re = fdt_mem_rsv(fdt, i)) != NULL; i++) {
 		if (fdt64_ld(&re->size) == 0)
 			return i;
@@ -208,117 +288,184 @@ int fdt_num_mem_rsv(const void *fdt)
 	return -FDT_ERR_TRUNCATED;
 }
 
+/*
+	nextprop_() : offset부터 PROP tag가 처음 등장할 때를 찾아서 offset을 반환
+*/
 static int nextprop_(const void *fdt, int offset)
 {
 	uint32_t tag;
 	int nextoffset;
 
 	do {
+		// 현재 offset의 다음 tag의 offset을 nextoffset에 저장
+		// 현재 offset의 tag를 tag에 저장
 		tag = fdt_next_tag(fdt, offset, &nextoffset);
 
 		switch (tag) {
+		// 현재 offset의 tag가 END일때,
+		// fdt_next_tag() 결과 nextoffset에 저장된 값이 
 		case FDT_END:
+			// >= 이면, BADSTRUCTURE 에러를 return
 			if (nextoffset >= 0)
 				return -FDT_ERR_BADSTRUCTURE;
+			// < 이면 fdt_next_tag()에서 저장된 에러 값을 return
 			else
 				return nextoffset;
 
+		// 현재 offset의 tag가 PROP 일때,
 		case FDT_PROP:
 			return offset;
 		}
+
+		// 다음 offset으로 이동
 		offset = nextoffset;
+	// 모든 tag를 다 검사
 	} while (tag == FDT_NOP);
 
+	// 찾지 못했으면 NOTFOUND 에러 return
 	return -FDT_ERR_NOTFOUND;
 }
 
+/*
+	fdt_subnode_offset_namelen() : offset의 자식 노드에서
+									 name과 namelen을 가지는 노드의 offset 반환
+*/
 int fdt_subnode_offset_namelen(const void *fdt, int offset,
 			       const char *name, int namelen)
 {
 	int depth;
-
+	// read only 확인
 	FDT_RO_PROBE(fdt);
 
-	for (depth = 0;
-	     (offset >= 0) && (depth >= 0);
-	     offset = fdt_next_node(fdt, offset, &depth))
-		if ((depth == 1)
-		    && fdt_nodename_eq_(fdt, offset, name, namelen))
+	// depth = 0 부터시작
+
+	// 현재 offset 부터 fdt_next_node()로 구한 offset이 존재하고,(다음 노드가 존재함)
+	// depth가 0 아래로 떨어지지 않을 때까지(부모 노드로 올라가지 않고, 자식 노드에서만 찾겠다)
+	
+	for (depth = 0; (offset >= 0) && (depth >= 0); offset = fdt_next_node(fdt, offset, &depth))
+		// 자식 노드이고, 그 노드의 node name이 name, namelen과 일치할 때
+		if ((depth == 1) && fdt_nodename_eq_(fdt, offset, name, namelen))
+			// fdt_next_node()에서 구한 다음 node의 offset을 반환
 			return offset;
 
+	// for문이 depth<0으로 종료됨
+	// depth < 0 이 되었다면 자식 노드에서 name과 namelen에 맞는 노드를 찾지 못한것
 	if (depth < 0)
+		// NOTFOUND 에러 return
 		return -FDT_ERR_NOTFOUND;
+
+	// for문이 offset < 0으로 종료됨
+	// fdt_next_node()이 반환한 값이 범위를 넘어갔거나, END일 때,
+	// 루트 노드에서 END_NODE를 만났을 때 음수값이 반환됨
 	return offset; /* error */
 }
 
+/*
+	fdt_subnode_offset() : parentoffset의 자식노드에서 name에 일치하는 node를 찾아 offset을 반환
+*/
 int fdt_subnode_offset(const void *fdt, int parentoffset,
 		       const char *name)
 {
 	return fdt_subnode_offset_namelen(fdt, parentoffset, name, strlen(name));
 }
 
+/*
+	fdt_path_offset_namelen() : path와 path의 길이인 namelen을 통해서 offset을 찾음
+*/
 int fdt_path_offset_namelen(const void *fdt, const char *path, int namelen)
 {
+	// path의 끝을 가리키는 end 포인터 
 	const char *end = path + namelen;
+	// path의 시작을 가리키는 p 포인터
 	const char *p = path;
 	int offset = 0;
 
+	// read only 확인
 	FDT_RO_PROBE(fdt);
 
 	/* see if we have an alias */
+	// path의 시작이 '/'이 아니면 alias가 있을 가능성이 있음
 	if (*path != '/') {
+		// path에서 path의 길이(end-p) 만큼 '/'를 찾아서 q가 가리키도록 함
 		const char *q = memchr(path, '/', end - p);
 
+		// '/'를 찾지 못했다면 q 는 NULL을 반환하므로
 		if (!q)
+			// path의 끝을 가리키도록 함
 			q = end;
 
+		// p의 alias가 있는지 확인
 		p = fdt_get_alias_namelen(fdt, p, q - p);
+		// alias가 없거나,
+		// alias는 있지만  p 이름이 aliases에 없을 때
 		if (!p)
 			return -FDT_ERR_BADPATH;
+		// alias p가 가리키는 path로 offset 반환
 		offset = fdt_path_offset(fdt, p);
 
+		// p가 '/' 다음을 가리키도록 함
 		p = q;
 	}
 
+	// p가 end까지 반복
 	while (p < end) {
 		const char *q;
 
+		// p 에서 '/' 제거
 		while (*p == '/') {
 			p++;
+			// p 가 끝을 가리키면, 0 반환
 			if (p == end)
 				return offset;
 		}
+		// '/'를 찾아 q가 가리키도록 함
 		q = memchr(p, '/', end - p);
+		// '/'를 찾지 못한 경우에는 path의 끝(end)를 가리키도록 함
 		if (! q)
 			q = end;
 
+		// offset의 자식노드에서 이름이 p이고, 길이가 q-p인 노드의 offset을 반환
 		offset = fdt_subnode_offset_namelen(fdt, offset, p, q-p);
+		// 자식노드에서 찾지 못한 경우 에러 return
 		if (offset < 0)
 			return offset;
 
+		// p를 '/'를 가리키고 있는 q로 업데이트
 		p = q;
 	}
-
+	
+	// path의 마지막 경로까지의 offset을 반환
 	return offset;
 }
 
+/*
+	fdt_path_offset() : path 경로로 offset을 찾아서 반환
+*/
 int fdt_path_offset(const void *fdt, const char *path)
 {
 	return fdt_path_offset_namelen(fdt, path, strlen(path));
 }
 
+/*
+	fdt_get_name() : nodeoffset에 해당하는 이름을 return하고 길이를 len에 저장
+*/
 const char *fdt_get_name(const void *fdt, int nodeoffset, int *len)
 {
+	// nodeoffset이 가리키는 노드의 포인터를 반환
 	const struct fdt_node_header *nh = fdt_offset_ptr_(fdt, nodeoffset);
 	const char *nameptr;
 	int err;
 
+	// read only인지 확인
+	// nodeoffset가 음수인지, tag size로 정렬되어 있는지, BEGIN_NODE를 가리키고 있는지 확인
 	if (((err = fdt_ro_probe_(fdt)) != 0)
 	    || ((err = fdt_check_node_offset_(fdt, nodeoffset)) < 0))
 			goto fail;
 
+	// nameptr가 노드 이름을 가리키게 함
 	nameptr = nh->name;
 
+	// 버전 확인
 	if (fdt_version(fdt) < 0x10) {
 		/*
 		 * For old FDT versions, match the naming conventions of V16:
@@ -326,64 +473,96 @@ const char *fdt_get_name(const void *fdt, int nodeoffset, int *len)
 		 * contents are loosely checked.
 		 */
 		const char *leaf;
+		// nameptr 오른쪽 끝에서 부터 '/'를 찾아서 leaf가 가리키도록 함
 		leaf = strrchr(nameptr, '/');
+		// '/'를 찾지 못했다면 에러
 		if (leaf == NULL) {
 			err = -FDT_ERR_BADSTRUCTURE;
 			goto fail;
 		}
+		// nameptr이 '/' 다음을 가리키도록 함
 		nameptr = leaf+1;
 	}
 
+	// len이 NULL이 아니면
+	// nameptr의 길이를 저장
 	if (len)
 		*len = strlen(nameptr);
-
+	// nameptr을 return
 	return nameptr;
 
+// nodeoffset 이상하거나, '/'를 찾지 못했을 때
  fail:
+	// len이 존재하면, err를 저장
 	if (len)
 		*len = err;
+	// NULL 반환
 	return NULL;
 }
 
+/*
+	fdt_first_property_offset() : nodeoffset이 가리키는 노드에서 첫번째 property offset을 반환
+*/
 int fdt_first_property_offset(const void *fdt, int nodeoffset)
 {
 	int offset;
 
+	// nodeoffset이 음수인지, 정렬이 잘 되어 있는지, BEGIN_NODE를 가리키고 있는지 확인
 	if ((offset = fdt_check_node_offset_(fdt, nodeoffset)) < 0)
 		return offset;
 
+	// offset으로 부터 첫번째 PROP를 찾아서 offset 반환
 	return nextprop_(fdt, offset);
 }
 
+/*
+	fdt_next_property_offset() : offset 다음의 property offset을 반환
+*/
 int fdt_next_property_offset(const void *fdt, int offset)
 {
+	// offset이 음수인지, 정렬이 잘 되어있는지, PROP을 가리키고 있는지 확인
 	if ((offset = fdt_check_prop_offset_(fdt, offset)) < 0)
 		return offset;
 
+	// offset으로 부터 다음 PROP을 찾아서 offset 반환
 	return nextprop_(fdt, offset);
 }
 
+/*
+	fdt_get_property_by_offset_() : offset이 가라키는 PROP의 fdt_property 구조체를 반환
+									lenp가 가리키는 곳에 len변수 저장
+*/
 static const struct fdt_property *fdt_get_property_by_offset_(const void *fdt,
 						              int offset,
 						              int *lenp)
 {
 	int err;
+	// fdt_property 구조체를 가리키는 prop 포인터 선언
 	const struct fdt_property *prop;
 
+	// offset이 음수인지, 정렬이 되어있는지, PROP을 가리키고 있는지 확인
 	if ((err = fdt_check_prop_offset_(fdt, offset)) < 0) {
+		// lenp가 NULL이 아니면, 에러를 저장하고 NULL 리턴
 		if (lenp)
 			*lenp = err;
 		return NULL;
 	}
 
+	// structure block에서 offset 만큼의 오프셋을 가지는 포인터 반환
 	prop = fdt_offset_ptr_(fdt, offset);
 
+	// lenp가 NULL이 아니면, 엔디안을 고려하여 fdt_property 구조체의 len을 저장
 	if (lenp)
 		*lenp = fdt32_ld(&prop->len);
 
+	// fdt_property 구조체를 가리키는 포인터 반환
 	return prop;
 }
 
+/*
+	fdt_get_property_by_offset() : structure block에서 offset이 가리키는 곳의
+									property를 가리키는 fdt_property 구조체 반환
+*/
 const struct fdt_property *fdt_get_property_by_offset(const void *fdt,
 						      int offset,
 						      int *lenp)
@@ -391,15 +570,22 @@ const struct fdt_property *fdt_get_property_by_offset(const void *fdt,
 	/* Prior to version 16, properties may need realignment
 	 * and this API does not work. fdt_getprop_*() will, however. */
 
+	// 16버전 이전에서는 이 함수를 사용하지 않고, fdt_getprop_() 함수를 사용
 	if (fdt_version(fdt) < 0x10) {
 		if (lenp)
 			*lenp = -FDT_ERR_BADVERSION;
 		return NULL;
 	}
-
+	
+	// fdt_get_property_by_offset_() 함수 호출
 	return fdt_get_property_by_offset_(fdt, offset, lenp);
 }
 
+/*
+	fdt_get_property_namelen_() : offset이 가리키는 노드에서 name, namelen과 일치하는 property를 									찾아서 fdt_property 구조체를 포인터를 반환
+								-lenp가 가리키는 곳에는 property를 찾지못했을 때 에러 저장
+								-poffset에는 찾은 property의 offset을 저장
+*/
 static const struct fdt_property *fdt_get_property_namelen_(const void *fdt,
 						            int offset,
 						            const char *name,
@@ -407,29 +593,41 @@ static const struct fdt_property *fdt_get_property_namelen_(const void *fdt,
 							    int *lenp,
 							    int *poffset)
 {
+	// offset이 가리키는 노드의 첫번째 property 부터
+	// offset 다음 property 가 존재할 때까지 반복
 	for (offset = fdt_first_property_offset(fdt, offset);
 	     (offset >= 0);
 	     (offset = fdt_next_property_offset(fdt, offset))) {
 		const struct fdt_property *prop;
 
+		// offset이 음수인지, 정렬이 잘 되어있는지, PROP을 가리키고 있는지 확인
 		if (!(prop = fdt_get_property_by_offset_(fdt, offset, lenp))) {
 			offset = -FDT_ERR_INTERNAL;
 			break;
 		}
+		
+		// prop->nameoff을 통해 구해진 이름과 name, namelen에 일치하는 지 확인
 		if (fdt_string_eq_(fdt, fdt32_ld(&prop->nameoff),
 				   name, namelen)) {
+			// poffset이 NULL이 아니면, offset 값을 저장
 			if (poffset)
 				*poffset = offset;
+			// fdt_property를 가리키는 prop 리턴
 			return prop;
 		}
 	}
 
+	// offset이 가리키는 노드내에서 name, namelen에 해당하는 property를 찾지 못했을때,
+	// 에러를 lenp에 저장하고, NULL을 반환
 	if (lenp)
 		*lenp = offset;
 	return NULL;
 }
 
-
+/*
+	fdt_get_property_namelen() : offset이 가리키는 노드에서 name, namelen을 이용해 
+								property를 찾아 fdt_property 구조체를 반환
+*/
 const struct fdt_property *fdt_get_property_namelen(const void *fdt,
 						    int offset,
 						    const char *name,
@@ -437,17 +635,22 @@ const struct fdt_property *fdt_get_property_namelen(const void *fdt,
 {
 	/* Prior to version 16, properties may need realignment
 	 * and this API does not work. fdt_getprop_*() will, however. */
+	// 이 함수는 16버전 아래에서 사용할 수 없음
+	// fdt_getprop_()을 사용해야함.
 	if (fdt_version(fdt) < 0x10) {
 		if (lenp)
 			*lenp = -FDT_ERR_BADVERSION;
 		return NULL;
 	}
-
-	return fdt_get_property_namelen_(fdt, offset, name, namelen, lenp,
-					 NULL);
+	
+	// fdt_get_property_namelen_() 함수 호출
+	return fdt_get_property_namelen_(fdt, offset, name, namelen, lenp, NULL);
 }
 
-
+/*
+	fdt_get_property() : fdt_get_property_namelen 래퍼 함수, 
+						name의 길이를 strlen(name)으로 구함
+*/
 const struct fdt_property *fdt_get_property(const void *fdt,
 					    int nodeoffset,
 					    const char *name, int *lenp)
@@ -456,21 +659,33 @@ const struct fdt_property *fdt_get_property(const void *fdt,
 					strlen(name), lenp);
 }
 
+/*
+	fdt_getprop_namelen() : nodeoffset에 해당하는 노드에서 name, namelen에 일치하는
+						 fdt_property가 가지고 있는 data를 반환
+							- 버전을 
+*/
 const void *fdt_getprop_namelen(const void *fdt, int nodeoffset,
 				const char *name, int namelen, int *lenp)
 {
 	int poffset;
 	const struct fdt_property *prop;
 
+	// nodeoffset에 해당하는 노드에서 name, namelen과 일치하는 fdt_property 구조체를 반환
 	prop = fdt_get_property_namelen_(fdt, nodeoffset, name, namelen, lenp,
 					 &poffset);
+	// name, namelen에 일치하는 fdt_property 구조체를 찾지 못한 경우, NULL 반환
 	if (!prop)
 		return NULL;
 
 	/* Handle realignment */
+	// fdt버전이 16아래이고
+	// 찾은 fdt_property 구조체가 8 바이트 단위로 정렬되어 있고
+	// prop->len 이 8보다 크면 prop->data + 4를 반환
 	if (fdt_version(fdt) < 0x10 && (poffset + sizeof(*prop)) % 8 &&
 	    fdt32_ld(&prop->len) >= 8)
 		return prop->data + 4;
+
+	// prop->data를 반환
 	return prop->data;
 }
 
@@ -530,10 +745,13 @@ const char *fdt_get_alias_namelen(const void *fdt,
 {
 	int aliasoffset;
 
+	// "/aliases"로 offset을 구함
 	aliasoffset = fdt_path_offset(fdt, "/aliases");
+	// "/aliases"가 없을 때, NULL 리턴
 	if (aliasoffset < 0)
 		return NULL;
 
+	// aliasoffset에서 name과 namelen으로 PROP를 찾아서 data를 반환
 	return fdt_getprop_namelen(fdt, aliasoffset, name, namelen, NULL);
 }
 
@@ -909,8 +1127,7 @@ int fdt_check_full(const void *fdt, size_t bufsize)
 			break;
 
 		case FDT_PROP:
-			prop = fdt_getprop_by_offset(fdt, offset, &propname,
-						     &err);
+			prop = fdt_getprop_by_offset(fdt, offset, &propname, &err);
 			if (!prop)
 				return err;
 			break;
